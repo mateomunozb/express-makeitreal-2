99a14a11fdc3bb4a1bed14d7117682249a20f0f7
root <root@DESKTOP-4I5C2KQ.localdomain>
2020-08-11 19:20:25 -0500

    Correcciones

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1><div class="secret" style="display: none;">HcUy6Re2LLBRtj</div></h1>')
+  res.send('<h1>HcUy6Re2LLBRt</h1>')
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
